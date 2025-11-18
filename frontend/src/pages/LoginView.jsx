1dd79ad4e965289f03f8b1d48a7280b524698e45
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 13:48:00 -0500

    added handler functions and state

---
+++ frontend/src/pages/LoginView.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../store/slices/authSlice.js";
+
+function LoginView() {
+  const [isRegister, setIsRegister] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  // form submission handler for login / register
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+
+    const userData = { email, password };
+    if (isRegister) {
+      userData.username = username; //include user for registration
+    }
+    try {
+      let responseData;
+      if (isRegister) {
+        // call the register API service
+        responseData = await register(userData);
+        setMessage("registration successful! logging you in...");
+      } else {
+        // call the login API service
+        responseData = await login(userData);
+        setMessage("login successful!");
+      }
+
+      // if the request succeeds, dispatch the user data to redux and local storage
+      dispatch(loginSuccess(responseData));
+    } catch (error) {
+      // handle api errors
+      const errorMessage =
+        error.response?.data?.message ||
+        "an error occurred. please check your credentials.";
+      setMessage(errorMessage);
+      console.error("auth error:", error);
+    }
+  };
+}
