07fbd3dddc39701f66b96df762c57526c67ab2ba
Alexandra <alexandra.blandon@gmail.com>
2025-11-17 20:20:06 -0500

    added all imports to server file

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectdb from "./config/db.js";
+import cors from "cors";
+import recipe_routes from "./routes/recipeRoutes.js";
