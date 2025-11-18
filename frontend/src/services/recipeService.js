6fc1370d10dafb2b9010120345b94cc4325e9c94
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 10:08:36 -0500

    imports and created search function

--- frontend/src/services/recipeService.js
+++ frontend/src/services/recipeService.js
@@ -1,2 +1,10 @@
 //API Integration: Handles all client-server communication.
 // formattinfg data/ making HTTP requests backend API endpoints
+
+import axios from "axios";
+
+export const searchRecipes = async (query) => {
+  // search external recipes via wrapper
+  const response = await axios.get(`/api/recipes/search?query=${query}`); // request to backend search
+  return response.data; // returns recipe array results
+};
