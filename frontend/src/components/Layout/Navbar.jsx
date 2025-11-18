50db5fd23de9759f4ef31d91ae640eab97091f9d
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 11:12:52 -0500

    added navbar functions

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,3 +1,14 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/slices/authSlice";
+
+function Navbar() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch(); // hook to send actions to redux
+  const navigate = useNavigate(); // hook to change routes
+
+  function handleLogout() {
+    dispatch(logout()); //triggers logout action
+    navigate("/login"); // redirects user to login page
+  }
+}
