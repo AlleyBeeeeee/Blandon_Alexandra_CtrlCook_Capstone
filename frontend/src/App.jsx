3b108c712b77a941787111336d5c91357e7ce1f3
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 11:44:08 -0500

    added routes and navbar to app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./components/Layout/Navbar";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <Routes>
+          {/* routes for user authentication. */}
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/register" element={<LoginView />} />
+          {/* catch-all route (path="*") displays the notfound component for any unmatched url */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
