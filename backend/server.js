49ea60ec3b383f3674053328e2a4e0ae7842d5eb
Alexandra <alexandra.blandon@gmail.com>
2025-11-17 20:22:28 -0500

    iniated express and cors/middleware

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,8 @@ import recipe_routes from "./routes/recipeRoutes.js";
 
 dotenv.config();
 connectdb();
+
+const app = express();
+// allows frontend (port 5173) to communicate with backend (port 5000)
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
