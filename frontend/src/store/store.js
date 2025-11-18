6f4dc314840e37004abae1d0a746218cf8fd5092
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 09:58:37 -0500

    imported reducer and exported store

---
+++ frontend/src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice.js";
+
+export const store = configureStore({
+  // configures / exports central redux store
+  reducer: {
+    auth: authReducer, // assigns the authreducer to 'auth' key in store state
+  },
+});
