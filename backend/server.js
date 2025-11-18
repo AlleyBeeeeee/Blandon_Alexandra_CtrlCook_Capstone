923e5998188a681c5bee84a09ffb6aa386b470dc
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 15:13:43 -0500

    update server file for error handling

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,29 @@ import dotenv from "dotenv";
 import connectdb from "./config/db.js";
 import cors from "cors";
 import recipeRoutes from "./routes/recipeRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectdb();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 // allows frontend (port 5173) to communicate with backend (port 5000)
+
+//middleware
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // body parser for form data
 
 // recipe route integration
-app.use("/api/recipes", recipeRoutes);
+app.use("/api/recipes", recipeRoutes); // mounts recipe routes (CRUD & Search)
+app.use("/api/users", userRoutes); // mounts user routes (Register & Login)
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "backend api is running successfully on port 5000!",
-  });
-});
+// middleware error handler (must be after routes)
+app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+//
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
