82032a942c78b76403fb5bb7d149c78b7eb7e85a
Alexandra <alexandra.blandon@gmail.com>
2025-11-18 10:58:58 -0500

    fixed get issues

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import connectdb from "./config/db.js";
 import cors from "cors";
-import recipe_routes from "./routes/recipeRoutes.js";
+import recipeRoutes from "./routes/recipeRoutes.js";
 
 dotenv.config();
 connectdb();
+const port = process.env.PORT || 5000;
 
 const app = express();
 // allows frontend (port 5173) to communicate with backend (port 5000)
@@ -13,14 +14,14 @@ app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 // recipe route integration
-app.use("/api/recipes", recipe_routes);
+app.use("/api/recipes", recipeRoutes);
 
-// verify server is running
-app.get("/api/test", (req, res) => {
-  res.json({ message: "backend api is running successfully on port 5000!" });
+app.get("/", (req, res) => {
+  res.json({
+    message: "backend api is running successfully on port 5000!",
+  });
 });
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
