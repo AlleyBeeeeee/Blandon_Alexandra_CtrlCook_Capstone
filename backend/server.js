dfe2f426986aa0293bdc3efaa8b690f002213a93
Alexandra <alexandra.blandon@gmail.com>
2025-11-17 20:21:18 -0500

    initiated mongodb connection and dotenv

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,6 @@ import dotenv from "dotenv";
 import connectdb from "./config/db.js";
 import cors from "cors";
 import recipe_routes from "./routes/recipeRoutes.js";
+
+dotenv.config();
+connectdb();
