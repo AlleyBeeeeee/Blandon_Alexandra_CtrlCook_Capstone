7b71ded2586be1821cf83030de933184b40c8642
Alexandra <alexandra.blandon@gmail.com>
2025-11-17 20:26:56 -0500

    port connection

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,8 @@ app.use("/api/recipes", recipe_routes);
 app.get("/api/test", (req, res) => {
   res.json({ message: "backend api is running successfully on port 5000!" });
 });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
